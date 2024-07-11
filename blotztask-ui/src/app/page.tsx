ec17c6209e4ea6a7b7e50a3db12e3994c524f4ae
benneoh <benjaminneoh6343@gmail.com>
2024-07-11 22:31:07 +1000

    test cicd pipeline

--- blotztask-ui/src/app/page.tsx
+++ blotztask-ui/src/app/page.tsx
@@ -90,7 +90,12 @@ export default function Home() {
             Explore starter templates for Next.js.
           </p>
         </a>
-
+        <h2 className="mb-3 text-2xl font-semibold">
+            Templates{" "}
+            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
+              -&gt;
+            </span>
+          </h2>
         <a
           href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
           className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
