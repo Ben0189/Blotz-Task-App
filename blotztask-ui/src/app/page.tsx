36de403d2a6daaac1be1e71d3bb7c6e9dff39426
benneoh <benjaminneoh6343@gmail.com>
2024-07-11 22:24:19 +1000

    test frontend commit

--- blotztask-ui/src/app/page.tsx
+++ blotztask-ui/src/app/page.tsx
@@ -86,6 +86,12 @@ export default function Home() {
               -&gt;
             </span>
           </h2>
+          <h2 className="mb-3 text-2xl font-semibold">
+            Templates{" "}
+            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
+              -&gt;
+            </span>
+          </h2>
           <p className="m-0 max-w-[30ch] text-sm opacity-50">
             Explore starter templates for Next.js.
           </p>
