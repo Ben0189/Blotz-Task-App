024812353e7b93ef481a71dee461583b3960ad56
benjaminneoh <benjaminneoh6343@gmail.com>
2024-07-25 00:07:31 +1000

    Add db context with Taskitem table

---
+++ blotztask-api/Data/BlotzTaskDbContext.cs
@@ -0,0 +1,19 @@
+﻿using BlotzTask.Data.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace BlotzTask.Data
+{
+    public class BlotzTaskDbContext : DbContext
+    {
+        public BlotzTaskDbContext(DbContextOptions options) : base(options) {}
+        public DbSet<TaskItem> TaskItems { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<TaskItem>().HasData(
+                new TaskItem { Id = 1, Title = "Initial Task 1" },
+                new TaskItem { Id = 2, Title = "Initial Task 2" },
+                new TaskItem { Id = 3, Title = "Initial Task 3" }
+            );
+        }
+    }
+}
