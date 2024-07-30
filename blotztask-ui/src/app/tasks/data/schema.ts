5aab0aeda58f55308a12fdef9473e6a760a13dff
benjaminneoh <benjaminneoh6343@gmail.com>
2024-07-30 23:36:20 +1000

    initial implement day view page

--- blotztask-ui/src/app/tasks/data/schema.ts
+++ blotztask-ui/src/app/tasks/data/schema.ts
@@ -1,7 +1,6 @@
 import { z } from "zod"
 
-// We're keeping a simple non-relational schema here.
-// IRL, you will have a schema for your data models.
+//TODO : This will be remove once backend is ready, this schema is not what we want for our application
 export const taskSchema = z.object({
   id: z.string(),
   title: z.string(),
@@ -11,3 +10,15 @@ export const taskSchema = z.object({
 })
 
 export type Task = z.infer<typeof taskSchema>
+
+// this is the schema we want for our application
+export const taskDTOSchema = z.object({
+  id: z.number(),
+  title: z.string(),
+  description: z.string(),
+  isDone: z.boolean(),
+  createdAt: z.string().transform((str) => new Date(str)), // Convert string to Date
+  updatedAt: z.string().transform((str) => new Date(str)), // Convert string to Date
+});
+
+export type TaskDTO = z.infer<typeof taskDTOSchema>;
