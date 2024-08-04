93755ac5596e9a5cfecb132b735d327779dfbd01
Wendiiiit <tangruran@gmail.com>
2024-08-04 21:10:21 +1000

    Changed the description on coming soon page.

--- blotztask-ui/src/app/coming-soon/page.tsx
+++ blotztask-ui/src/app/coming-soon/page.tsx
@@ -7,7 +7,7 @@ export default function Home() {
     <main className="flex flex-col gap-5 p-12 md:items-center md:p-28">
       <H1>⚒️ Coming Soon...</H1>
       <H3 className="text-lg font-light text-muted-foreground sm:text-xl">
-        We are currently working on this page. Please check back later.
+        We are currently working on this page. Please come back to us later. 
       </H3>
       <div className="mt-16 flex flex-col gap-3 md:flex-row">
         <Link href="/">
