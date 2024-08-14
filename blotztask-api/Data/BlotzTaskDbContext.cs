45d65a33ae199aa25c35364089c026212d60f28e
sguming <shangguming@gmail.com>
2024-08-15 07:21:21 +0930

    Add new colmn in entity and add example data for Database context

--- blotztask-api/Data/BlotzTaskDbContext.cs
+++ blotztask-api/Data/BlotzTaskDbContext.cs
@@ -1,18 +1,45 @@
 ﻿using BlotzTask.Data.Entities;
 using Microsoft.EntityFrameworkCore;
+using System;
 
 namespace BlotzTask.Data
 {
     public class BlotzTaskDbContext : DbContext
     {
-        public BlotzTaskDbContext(DbContextOptions options) : base(options) {}
+        public BlotzTaskDbContext(DbContextOptions options) : base(options) { }
+
         public DbSet<TaskItem> TaskItems { get; set; }
+
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             modelBuilder.Entity<TaskItem>().HasData(
-                new TaskItem { Id = 1, Title = "Initial Task 1" },
-                new TaskItem { Id = 2, Title = "Initial Task 2" },
-                new TaskItem { Id = 3, Title = "Initial Task 3" }
+                new TaskItem
+                {
+                    Id = 1,
+                    Title = "Initial Task 1",
+                    Description = "Description for Task 1",
+                    IsDone = false,
+                    CreatedAt = DateTime.UtcNow,
+                    UpdatedAt = DateTime.UtcNow
+                },
+                new TaskItem
+                {
+                    Id = 2,
+                    Title = "Initial Task 2",
+                    Description = "Description for Task 2",
+                    IsDone = false,
+                    CreatedAt = DateTime.UtcNow,
+                    UpdatedAt = DateTime.UtcNow
+                },
+                new TaskItem
+                {
+                    Id = 3,
+                    Title = "Initial Task 3",
+                    Description = "Description for Task 3",
+                    IsDone = false,
+                    CreatedAt = DateTime.UtcNow,
+                    UpdatedAt = DateTime.UtcNow
+                }
             );
         }
     }
