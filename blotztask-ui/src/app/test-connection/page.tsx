a34f8b147e3e0686b4b69a0f58e524c5804aa689
cindyTAT <wanchengt@student.unimelb.edu.au>
2024-08-16 19:23:57 +1000

    Test connectiong page added cards

--- blotztask-ui/src/app/test-connection/page.tsx
+++ blotztask-ui/src/app/test-connection/page.tsx
@@ -4,6 +4,7 @@ import { Button } from "@/components/ui/button";
 import { H1, H3 } from "@/components/ui/heading-with-anchor";
 import Link from "next/link";
 import { useState } from "react";
+import { Card, CardHeader, CardTitle, CardDescription, CardContent } from "@/components/ui/card";
 
 export default function Home() {
 
@@ -30,7 +31,7 @@ export default function Home() {
   
   return (
     <main className="flex flex-col gap-5 p-12 md:items-center md:p-28">
-    <H1>⚒️ Test Backend Connection</H1>
+    <H1>📡 Test Server Connection</H1>
     <H3 className="text-lg font-light text-muted-foreground sm:text-xl">
       Click the button to fetch the current list of todo items.
     </H3>
@@ -45,16 +46,32 @@ export default function Home() {
       </Link>
     </div>
     {error && (
-      <p className="mt-8 text-red-500">
-        Error fetching todos: {error}
-      </p>
+      <Card className="mt-8 max-w-xs">
+        <CardHeader>
+          <CardTitle>
+            <span className="text-xl">❌</span> Error fetching todos:
+          </CardTitle>
+        </CardHeader>
+        <CardContent>
+          <p>{error}</p>
+        </CardContent>
+      </Card>
     )}
     {todos.length > 0 && (
-      <ul className="mt-8 list-disc">
-        {todos.map((todo, index) => (
-          <li key={index}>{todo.title}</li>
-        ))}
-      </ul>
+      <Card className="mt-8 max-w-xs">
+        <CardHeader>
+          <CardTitle>
+            <span className="text-xl">✅</span> Successfully fetched todos:
+          </CardTitle>
+        </CardHeader>
+        <CardContent>
+          <ul className="list-disc ml-5">
+            {todos.map((todo, index) => (
+              <li key={index}>{todo.title}</li>
+            ))}
+          </ul>
+        </CardContent>
+      </Card>
     )}
   </main>
   );
