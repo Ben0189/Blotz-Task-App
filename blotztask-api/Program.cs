491f7e8d8c75c17db1b36afe3cad7fb42a57a0c6
benjaminneoh <benjaminneoh6343@gmail.com>
2024-08-17 07:34:22 +1000

    setup db connection in prod using kv

--- blotztask-api/Program.cs
+++ blotztask-api/Program.cs
@@ -1,21 +1,43 @@
+using Azure.Identity;
+using Azure.Security.KeyVault.Secrets;
 using BlotzTask.Data;
 using BlotzTask.Services;
+using Microsoft.Azure.KeyVault;
+using Microsoft.Azure.Services.AppAuthentication;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration.AzureKeyVault;
+
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Services.AddControllers();
+builder.Services.AddControllers();  
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddScoped<ITaskService, TaskService>();
 
-var databaseConnectionString = builder.Configuration.GetConnectionString("DefaultConnection");
-builder.Services.AddDbContext<BlotzTaskDbContext>(options => options.UseSqlServer(databaseConnectionString));
+if (builder.Environment.IsDevelopment())
+{
+    var databaseConnectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+    builder.Services.AddDbContext<BlotzTaskDbContext>(options => options.UseSqlServer(databaseConnectionString));
+}
 
-builder.Services.AddCors(options =>
+if (builder.Environment.IsDevelopment())
+{
+    var keyVaultEndpoint = builder.Configuration.GetSection("KeyVault").GetValue<string>("VaultURI");
+
+    var keyVaultClient = new KeyVaultClient(new KeyVaultClient.AuthenticationCallback(new AzureServiceTokenProvider().KeyVaultTokenCallback));
+
+    builder.Configuration.AddAzureKeyVault(keyVaultEndpoint, new DefaultKeyVaultSecretManager());
+
+    var client = new SecretClient(new Uri(keyVaultEndpoint), new DefaultAzureCredential());
+    builder.Services.AddDbContext<BlotzTaskDbContext>(options => options.UseSqlServer(client.GetSecret("db-string-connection").Value.Value.ToString()));
+
+}
+
+    builder.Services.AddCors(options =>
 {
     options.AddPolicy("AllowSpecificOrigin",
         builder =>
