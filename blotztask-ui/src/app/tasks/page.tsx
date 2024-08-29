9ebca1990d8c00aff850ff77f5a23eca45aa0e31
Surui Liu <u7875420@anu.edu.zu>
2024-08-29 18:09:15 +0800

    tasks

--- blotztask-ui/src/app/tasks/page.tsx
+++ blotztask-ui/src/app/tasks/page.tsx
@@ -62,7 +62,7 @@ export default async function TaskPage() {
           <div>
             <h2 className="text-2xl font-bold tracking-tight">Task List</h2>
             <p className="text-muted-foreground">
-              Here&apos;s a list of your tasks for this month!
+              Here&apos;s a list of your tasks for this month! 
             </p>
           </div>
           <div className="flex items-center space-x-2">{/* <UserNav /> */}</div>
