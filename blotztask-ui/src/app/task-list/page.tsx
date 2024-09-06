a6419e42f1cab265d787391208ffac8ce3a65b88
Surui Liu <u7875420@anu.edu.zu>
2024-09-06 14:34:01 +0800

    create a mockdata file and seperate data with business logic

--- blotztask-ui/src/app/task-list/page.tsx
+++ blotztask-ui/src/app/task-list/page.tsx
@@ -1,50 +1,10 @@
 import { TaskTable } from './components/task-table';
-
-import { TaskItemDTO } from "@/model/task-Item-dto";
-  
-  const tasks : TaskItemDTO[] = [
-    {
-        id: 1,
-        title: "Complete project report",
-        isDone: false,
-    },
-    {
-        id: 2,
-        title: "Review pull requests",
-        isDone: true,
-    },
-    {
-        id: 3,
-        title: "Team meeting",
-        isDone: false,
-    },
-    {
-        id: 4,
-        title: "Update documentation",
-        isDone: true,
-    },
-    {
-        id: 5,
-        title: "Code review for module A",
-        isDone: false,
-    },
-    {
-        id: 6,
-        title: "Deploy new version",
-        isDone: true,
-    },
-    {
-        id: 7,
-        title: "Refactor authentication service",
-        isDone: false,
-    },
-];
-
+import { mocktasks } from './mockdata';
 
 const page = () => {
   return (
     <div>
-      <TaskTable tasks = {tasks}/>
+      <TaskTable tasks = {mocktasks}/>
     </div>
   )
 }
