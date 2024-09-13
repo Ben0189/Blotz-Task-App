2afd95b3c585529af95e1b99f9a8e8129b4cf91e
tech_ValAi <tech@valai.com.au>
2024-09-14 03:01:00 +0930

    Implement the controller file for label without the Service.cs

---
+++ blotztask-api/Controllers/LabelController.cs
@@ -0,0 +1,29 @@
+﻿using BlotzTask.Data;
+using BlotzTask.Data.Entities;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace BlotzTask.Controllers
+{
+    [ApiController]
+    [Authorize]
+    [Route("api/[controller]")]
+    public class LabelController : ControllerBase
+    {
+        private readonly BlotzTaskDbContext _labelContext;
+
+        public LabelController(BlotzTaskDbContext labelContext)
+        {
+            _labelContext = labelContext;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<Label>>> GetAllLabels()
+        {
+            var labels = await _labelContext.Labels.ToListAsync();
+            return Ok(labels);
+        }
+    }
+}
