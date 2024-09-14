3b8ab71bfb6a5b25fb15137ef946fb96d370c654
cloverluo112 <xinyueluo1999@gmail.com>
2024-09-15 01:21:59 +0930

    Just want to see if the contributor name has been updated

--- blotztask-api/Data/Entities/Label.cs
+++ blotztask-api/Data/Entities/Label.cs
@@ -2,6 +2,7 @@
 {
     public class Label
     {
+        
         public int LabelId { get; set; }
         public string Name { get; set; } = string.Empty;
         public string Color { get; set; } = string.Empty;
