9e7a4eaa0414fe6844f6d471ca7e0c6da7feee89
Harry Song <harry.song518@gmail.com>
2024-09-15 11:29:01 +0930

    feat: Add DueDate property to TaskItemDTO and TaskItem models

--- blotztask-api/Migrations/20240915015630_createDuedateToTaskItem.Designer.cs
+++ blotztask-api/Migrations/20240915015630_createDuedateToTaskItem.Designer.cs
@@ -4,6 +4,7 @@ using BlotzTask.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace BlotzTask.Migrations
 {
     [DbContext(typeof(BlotzTaskDbContext))]
-    partial class BlotzTaskDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240915015630_createDuedateToTaskItem")]
+    partial class createDuedateToTaskItem
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
