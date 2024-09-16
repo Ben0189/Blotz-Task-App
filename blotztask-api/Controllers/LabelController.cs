82de7477d4839c37b27b7928ad98de6d0bbf646c
Surui Liu <u7875420@anu.edu.zu>
2024-09-16 18:21:59 +0800

    Implement get label by id endpoint

---
+++ blotztask-api/Controllers/LabelController.cs
@@ -0,0 +1,27 @@
+﻿using BlotzTask.Services;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BlotzTask.Controllers
+{
+    [Route("api/[controller]")]
+    [Authorize]
+    [ApiController]
+    public class LabelController : ControllerBase
+    {
+        private readonly ILabelService _labelService;
+
+        public LabelController(ILabelService labelService)
+        {
+            _labelService = labelService;
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetLabelById(int id)
+        {
+            return Ok(await _labelService.GetLabelById(id)); // Return 200 OK with the label data
+        }
+        
+    }
+}
