1409af7a9a2b8a7afa2f575781e3e65e0b53b5e1
nets7snipper <53284908+nets7snipper@users.noreply.github.com>
2024-09-30 17:54:57 +0800

    # Add auto validation config & give a validation sample controller (#88)
    Co-authored-by: Ben Neoh [SSW] <87377903+Ben0189@users.noreply.github.com>

---
+++ blotztask-api/Controllers/SampleValidationController.cs
@@ -0,0 +1,31 @@
+﻿using BlotzTask.Data.Entities;
+using BlotzTask.Models;
+using FluentValidation;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace BlotzTask.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SampleValidationController : ControllerBase
+    {
+
+        // POST api/<SampleValidationController>
+        [HttpPost]
+        [AllowAnonymous]
+        public IActionResult CreateValidation(SampleValidationDTO dto)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            // Process the valid user model (e.g., save to a database)
+            return Ok("Created successfully!");
+        }
+
+    }
+}
