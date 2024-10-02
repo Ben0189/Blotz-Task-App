3bc85844d17356378dd3168dd376d26bb4e95a45
Ben Neoh [SSW] <87377903+Ben0189@users.noreply.github.com>
2024-10-02 20:56:17 +1000

    Backend error handling implementation (#102)

---
+++ blotztask-api/Models/ApiResponse/ApiResponse.cs
@@ -0,0 +1,8 @@
+namespace BlotzTask.Models.ApiResponse;
+
+public class ApiResponse<T>
+{
+    public bool Success { get; set; }
+    public string Message { get; set; } = string.Empty;
+    public T? Data { get; set; }
+}
