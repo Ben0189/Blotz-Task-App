be5b70f8fcd7b326d734f72f1b84fac6873a4315
benjaminneoh <benjaminneoh6343@gmail.com>
2024-10-02 21:46:32 +1000

    remove unused using

--- blotztask-api/Models/EditTaskItemDTO.cs
+++ blotztask-api/Models/EditTaskItemDTO.cs
@@ -1,17 +1,11 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
 namespace BlotzTask.Models
 {
     public class EditTaskItemDTO
-{
-    public string Title { get; set; }
-    public string Description { get; set; }
-    public DateOnly DueDate { get; set; } 
-    public bool IsDone { get; set; }
-    public DateTime UpdatedAt { get; set; }
-
-}
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public DateOnly DueDate { get; set; } 
+        public bool IsDone { get; set; }
+    }
 }
