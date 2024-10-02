5668dd054c83ebbce4ae61150427110e02471b59
Ben Neoh [SSW] <87377903+Ben0189@users.noreply.github.com>
2024-10-02 15:40:09 +1000

    Sample data seeding implementation (#101)

--- blotztask-api/Data/Entities/TaskItem.cs
+++ blotztask-api/Data/Entities/TaskItem.cs
@@ -13,5 +13,9 @@ namespace BlotzTask.Data.Entities
         public bool IsDone { get; set; }
         public DateTime CreatedAt { get; set; }
         public DateTime UpdatedAt { get; set; }
+        public string UserId { get; set; }
+        [ForeignKey("UserId")]
+        public User User { get; set; }
+
     }
 }
