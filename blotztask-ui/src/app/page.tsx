59535e4fa68f8951a61559059783c19c3b36a4ce
Ben Neoh [SSW] <87377903+Ben0189@users.noreply.github.com>
2024-10-08 23:36:37 +1100

    Setup color theme (#107)

--- blotztask-ui/src/app/page.tsx
+++ blotztask-ui/src/app/page.tsx
@@ -4,9 +4,9 @@ import { H1, H3 } from '@/components/ui/heading-with-anchor';
 export default function Home() {
   return (
     <main className="flex flex-col gap-5 p-12 md:items-center md:p-28">
-      <H1 className="green_gradient">
+      <H1 className="heading-primary">
         Blotz
-        <span className="blue_gradient text-center">Task</span>
+        <span className="heading-secondary">Task</span>
       </H1>
 
       <H3 className="text-lg font-light text-muted-foreground sm:text-xl">
--- blotztask-ui/tailwind.config.ts
+++ blotztask-ui/tailwind.config.ts
@@ -7,22 +7,20 @@ const config: Config = {
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
     './src/**/*.{js,ts,jsx,tsx,mdx}',
     './app/**/*.{js,ts,jsx,tsx,mdx}',
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
     './components/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
       fontFamily: {
         satoshi: ['Satoshi', 'sans-serif'],
         inter: ['Inter', 'sans-serif'],
+        display: 'fantasy'
       },
       colors: {
-        'primary-orange': '#FF5722',
+        'primary-light': '#DBE4E9',
+        'primary-dark': '#2C3233',
+        'secondary': '#278291',
+        'warn': '#F42F67',
       }
     },
   },
