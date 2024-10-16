a0295468c7734d4161a72de756290882921edabc
unknown <Peter1031734590@gmail.com>
2024-10-16 22:24:14 +1100

    Added fetch task list for All task page and left a setUpdate function for future add item

--- blotztask-ui/src/app/task-list/page.tsx
+++ blotztask-ui/src/app/task-list/page.tsx
@@ -1,23 +1,40 @@
+'use client';
+
+import { useState } from 'react';
+import { useEffect } from 'react';
 import { Button } from '@/components/ui/button';
 import Link from 'next/link';
 import { AddTask } from './components/add-button';
 import { TaskTable } from './components/task-table';
-import { mocktasks } from './mockdata';
+import { TaskItemDTO } from '@/model/task-Item-dto';
+import { fetchAllTaskItems } from '@/services/taskService';
 
-const page = () => {
-  return (
-    <div className="flex flex-col items-end mt-10 mr-10">
-      <AddTask />
+export default function page() {
+    const [taskList, setTaskList] = useState<TaskItemDTO[]>([]);
+    const [update, setUpdate] = useState(false);
+    const loadTasks = async () => {
+        const data = await fetchAllTaskItems();
+        setTaskList(data);
+    }
 
-      <TaskTable tasks={mocktasks} />
+    /**
+     * Fetch the tasks once and set the hook on the first rendering
+     */
+    useEffect(() => {
+        loadTasks();
+    }, [update]); // Runs on the first render using [] parameter and rerun when state changes, e.g add task
 
-      <div className="mt-10">
-        <Button asChild className="ml-auto">
-          <Link href="/">Return Home Page</Link>
-        </Button>
-      </div>
-    </div>
-  );
-};
-
-export default page;
+    return (
+        <div className="flex flex-col items-end mt-10 mr-10">
+          <AddTask />
+    
+          <TaskTable tasks={taskList} />
+    
+          <div className="mt-10">
+            <Button asChild className="ml-auto">
+              <Link href="/">Return Home Page</Link>
+            </Button>
+          </div>
+        </div>
+      );
+}
