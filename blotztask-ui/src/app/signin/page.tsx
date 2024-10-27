01539e9d46f4276eba7891c01f43d54603106a9d
benjaminneoh <benjaminneoh6343@gmail.com>
2024-10-27 20:42:38 +1100

    fix css

--- blotztask-ui/src/app/signin/page.tsx
+++ blotztask-ui/src/app/signin/page.tsx
@@ -45,7 +45,7 @@ try {
   };
 
   return (
-    <div className="h-full justify-center flex flex-col gap-1 items-center">
+    <div className="h-full justify-center flex flex-col items-center">
       <div className="flex flex-col gap-4 bg-white p-5 rounded-lg shadow-md w-4/12">
         <h1 className={styles.title}>User Login</h1>{' '}
         {error &&
