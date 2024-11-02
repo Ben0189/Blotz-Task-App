fc783037a34a11484188ac29bf3dae0d6d913e9f
unknown <Peter1031734590@gmail.com>
2024-11-03 02:20:05 +1100

    added fetch service to get tasks due today

--- blotztask-ui/src/services/taskService.ts
+++ blotztask-ui/src/services/taskService.ts
@@ -24,7 +24,6 @@ export const fetchAllTaskItems = async (): Promise<TaskItemDTO[]> => {
 export const fetchTaskItemsDueToday = async (): Promise<TaskDTO[]> => {
     //Converting today's date to ISO String format
     const date = new Date().toISOString().split('T')[0];
-    // date = date.toISOString().split('T')[0];
 
     const response = await fetchWithAuth(
       `${process.env.NEXT_PUBLIC_API_BASE_URL_WITH_API}/Task/due-date/${date}`,
