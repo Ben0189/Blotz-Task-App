eb0989bdda3103ee252e2b50a427479bbec5437c
benjaminneoh <benjaminneoh6343@gmail.com>
2024-11-08 20:06:12 +1100

    remove all unused state after using react hook form

--- blotztask-ui/src/app/signin/page.tsx
+++ blotztask-ui/src/app/signin/page.tsx
@@ -2,7 +2,6 @@
 
 import { signIn } from 'next-auth/react';
 import { useRouter } from 'next/navigation';
-import { useState } from 'react';
 import styles from './AuthForm.module.css';
 import { Button } from '@/components/ui/button';
 import { Spinner } from '@/components/ui/spinner';
@@ -22,38 +21,8 @@ const LoginPage = () => {
     setError,
     formState : { errors, isSubmitting }  
   } = useForm<LoginFormField>();
+
   const router = useRouter(); // Get the router instance
-
-  const [email, setEmail] = useState(''); // State for email input
-  const [password, setPassword] = useState(''); // State for password input
-  // const [error, setError] = useState(null); // State for error message
-  // const [loading, setLoading] = useState(false); // State for loading spinner
-
-  // const handleSubmit = async (event) => {
-  //   event.preventDefault(); // Prevent default form submission
-
-  //   setLoading(true); // Start loading
-  //   setError(null); // Clear any previous errors
-
-  //   try {
-  //     const result = await signIn('credentials', {
-  //       redirect: false, // Prevent immediate redirect
-  //       email,
-  //       password,
-  //     });
-
-  //     if (result?.error) {
-  //       setError('Invalid credentials. Please try again.');
-  //     } else {
-  //       router.push('/task-dayview'); 
-  //     }
-  //   } catch (error) {
-  //     console.error('Login failed:', error);
-  //     setError('An unexpected error occurred. Please try again later.');
-  //   } finally {
-  //     setLoading(false); // Stop loading after processing
-  //   }
-  // };
 
   const onSubmit: SubmitHandler<LoginFormField> = async (formData) => {
     try {
@@ -82,12 +51,6 @@ const LoginPage = () => {
     <div className="h-full justify-center flex flex-col items-center">
       <div className="flex flex-col gap-4 bg-white p-5 rounded-lg shadow-md w-4/12">
         <h1 className={styles.title}>User Login</h1>
-        {/* {error &&
-          <AlertDestructive 
-            title="Error" 
-            description={error}
-          />
-        } */}
         {errors.root && (
           <AlertDestructive 
             title="Error" 
@@ -99,7 +62,6 @@ const LoginPage = () => {
             <label className={styles.label}>Email:</label>
             <input
               type="email"
-              value={email}
               {...register("email",{
                 required: "Email is required",
                 pattern: {
@@ -107,36 +69,33 @@ const LoginPage = () => {
                   message: "Entered value does not match email format"
                 }
               })}
-              onChange={(e) => setEmail(e.target.value)}
               required
               className={styles.input}
               placeholder="Enter your email"
             />
-            {errors.email && (
-              <div>{errors.email.message}</div>
-            )}
           </div>
+          {errors.email && (
+            <div className="text-warn mb-3">{errors.email.message}</div>
+          )}
           <div className={styles.input_group}>
             <label className={styles.label}>Password:</label>
-            <input
-              type="password"
-              value={password}
-              {...register("password",{
-                required: "Password is required",
-                minLength: {
-                  value : 9,
-                  message : "Password must have at least 9 character"
-                }
-              })}              
-              onChange={(e) => setPassword(e.target.value)}
-              required
-              className={styles.input}
-              placeholder="Enter your password"
-            />
-            {errors.password && (
-              <div>{errors.password.message}</div>
-            )}          
+              <input
+                type="password"
+                {...register("password",{
+                  required: "Password is required",
+                  minLength: {
+                    value : 9,
+                    message : "Password must have at least 9 character"
+                  }
+                })}              
+                required
+                className={styles.input}
+                placeholder="Enter your password"
+              />
           </div>
+          {errors.password && (
+              <div className="text-warn mb-3">{errors.password.message}</div>
+          )} 
           <Button className="w-full" type="submit" disabled={isSubmitting}>
             {isSubmitting ? <Spinner /> : 'Sign In'}
           </Button>
@@ -146,7 +105,6 @@ const LoginPage = () => {
           <a href="/signup" className={styles.registerLink}>
             Register here
           </a>  
-          {/* Registration link */}
         </p>
       </div>
     </div>
