ccf1411ae342977d89b51d6048bd3cce48407b50
Zhichen Ren <zhichenr@student.unimelb.edu.au>
2024-11-14 23:52:13 +1100

    #196 change to the new home page

--- blotztask-ui/src/app/page.tsx
+++ blotztask-ui/src/app/page.tsx
@@ -1,18 +1,16 @@
-import { H1, H3 } from '@/components/ui/heading-with-anchor';
+import { P } from '@/components/ui/heading-with-anchor';
 
 export default function Home() {
   return (
     <main className="flex flex-col gap-5 p-12 md:items-center md:p-28">
-      <H1 className="heading-primary">
-        Blotz
-        <span className="heading-secondary">Task</span>
-      </H1>
-
-      <H3 className="text-lg font-light text-muted-foreground sm:text-xl">
-        Designed to help users efficiently organize and track their tasks
-        providing functionality for task creation, management, and completion
-        tracking.
-      </H3>
+      <p className="w-[679px] h-[60px] absolute top-[197px] left-[402px] font-arial text-[32px] font-bold leading-[48px] tracking-[-0.41px] text-center text-[#278291]">
+        Welcome to the new age note taking web
+      </p>
+      <img
+        src="/assets/images/homePageNewPicture.png"
+        alt="an incredable image"
+        className="w-[849px] h-[350px] absolute top-[305px] left-[296px]"
+      />
     </main>
   );
 }
