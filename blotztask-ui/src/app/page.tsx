67106153eff792a94d69e159a059931f1300a4c3
benjaminneoh <benjaminneoh6343@gmail.com>
2024-11-16 19:22:59 +1100

    use text color variable

--- blotztask-ui/src/app/page.tsx
+++ blotztask-ui/src/app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <main className="flex flex-col gap-5 p-12 md:items-center md:p-28">
-      <p className="mb-6 font-arial text-[32px] font-bold leading-[48px] tracking-[-0.41px] text-center text-[#278291]">
+      <p className="mb-6 font-arial text-[32px] font-bold leading-[48px] tracking-[-0.41px] text-center text-secondary">
         Welcome to the new age note taking web
       </p>
       <img
