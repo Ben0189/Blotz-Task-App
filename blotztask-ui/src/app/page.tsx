90201ab8c6fba4f7046ab868d1858e8b50a18c73
benjaminneoh <benjaminneoh6343@gmail.com>
2024-11-16 19:24:56 +1100

    add use Image tag from Next

--- blotztask-ui/src/app/page.tsx
+++ blotztask-ui/src/app/page.tsx
@@ -1,10 +1,12 @@
+import Image from "next/image";
+
 export default function Home() {
   return (
     <main className="flex flex-col gap-5 p-12 md:items-center md:p-28">
       <p className="mb-6 font-arial text-[32px] font-bold leading-[48px] tracking-[-0.41px] text-center text-secondary">
         Welcome to the new age note taking web
       </p>
-      <img
+      <Image
         src="/assets/images/homePageNewPicture.png"
         alt="an incredable image"
         width={600}
