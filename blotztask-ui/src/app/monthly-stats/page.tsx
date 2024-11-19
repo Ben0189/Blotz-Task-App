aa1422419e2eb1b877bf96710c1a4d3b3320fa49
unknown <Peter1031734590@gmail.com>
2024-11-19 12:17:41 +1100

    add mock data for card components

--- blotztask-ui/src/app/monthly-stats/page.tsx
+++ blotztask-ui/src/app/monthly-stats/page.tsx
@@ -1,50 +1,77 @@
 'use client';
 
+import { Card } from '@/components/ui/card';
 import Image from 'next/image';
 import CountUp from 'react-countup';
+
 export default function Monthlystats() {
-  const mockTasks = [
+
+const mockTasks = [
     {
-      id: 1,
-      title: 'Complete project report',
-      description: 'Finalize the project report and submit it to the manager.',
-      isDone: false,
-      createdAt: '2024-07-20T08:30:00Z',
-      updatedAt: '2024-07-20T08:30:00Z',
+        id: 1,
+        title: 'Complete project report',
+        description: 'Finalize the project report and submit it to the manager.',
+        isDone: false,
+        createdAt: '2024-07-20T08:30:00Z',
+        updatedAt: '2024-07-20T08:30:00Z',
     },
     {
-      id: 2,
-      title: 'Meeting with the team',
-      description:
+        id: 2,
+        title: 'Meeting with the team',
+        description:
         'Discuss the project milestones and deadlines with the team.',
-      isDone: false,
-      createdAt: '2024-07-21T10:00:00Z',
-      updatedAt: '2024-07-21T10:00:00Z',
+        isDone: false,
+        createdAt: '2024-07-21T10:00:00Z',
+        updatedAt: '2024-07-21T10:00:00Z',
     },
-  ];
+];
+
+const mockLabels = {
+    "status": "success",
+    "data": {
+        "currentMonth": "2024-11",
+        "tasks": {
+            completed: {
+                "personal": 70,
+                "academic": 23,
+                "work": 28,
+            },
+            unfinished: {
+                "personal": 15,
+                "academic": 10,
+                "work": 5,
+            }
+        }
+    }
+};
 
   const taskCount = mockTasks.length;
   return (
-    <>
-      <div className="flex flex-col gap-5 md:items-center md:p-10">
-        <div className="mt-1">
-          <Image
-            src="/assets/images/profileImage.png"
-            alt="an incredable image"
-            width={150}
-            height={150}
-          />
-        </div>
         <div>
-          <p className="pl-20 font-arial font-bold text-[40px] leading-[100px] tracking-[-0.41px] text-center text-secondary">
-            You planned{' '}
-            <span className="font-arial font-bold text-[120px] leading-[100px] tracking-[-0.41px] text-center text-secondary">
-              <CountUp end={taskCount} />{' '}
-            </span>
-            tasks this month
-          </p>
+            <div className="flex flex-col gap-5 md:items-center md:p-10">
+                <div className="mt-1">
+                    <Image
+                        src="/assets/images/profileImage.png"
+                        alt="an incredable image"
+                        width={150}
+                        height={150}
+                    />
+                </div>
+                <div>
+                    <p className="pl-20 font-arial font-bold text-[40px] leading-[100px] tracking-[-0.41px] text-center text-secondary">
+                        You planned{' '}
+                        <span className="font-arial font-bold text-[120px] leading-[100px] tracking-[-0.41px] text-center text-secondary">
+                        <CountUp end={taskCount} />{' '}
+                        </span>
+                        tasks this month
+                    </p>
+                </div>
+            </div>
+            <div>
+                <Card>
+
+                </Card>
+            </div>
         </div>
-      </div>
-    </>
   );
 }
