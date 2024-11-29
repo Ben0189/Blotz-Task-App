33cd29630d5e711e0c50b6cc7b668ca5364d978a
Xiaoxiao Lin <jessylin187@gmail.com>
2024-11-30 02:30:46 +1100

    Import useForm and Zod & define Zod validation

--- blotztask-ui/src/app/signup/page.tsx
+++ blotztask-ui/src/app/signup/page.tsx
@@ -1,6 +1,8 @@
 'use client';
 
-import { useState } from 'react';
+import { useForm } from 'react-hook-form';
+import { z } from 'zod';
+import { zodResolver } from '@hookform/resolvers/zod';
 import styles from '../signin/AuthForm.module.css'; // Import CSS styles
 import { Button } from '@/components/ui/button';
 import { Spinner } from '@/components/ui/spinner';
@@ -10,12 +12,22 @@ import { BadRequestError } from '@/model/error/bad-request-error';
 import { useRouter } from 'next/navigation';
 import { toast } from 'sonner';
 
-const SignUpPage = () => {
-  const router = useRouter(); // Initialize router
-  const [email, setEmail] = useState(''); // State for email input
-  const [password, setPassword] = useState(''); // State for password input
-  const [error, setError] = useState(null); // State for error message
-  const [loading, setLoading] = useState(false); // State for loading spinner
+//Define Zod validation schema
+const signUpSchema = z.object({
+  email: z
+    .string()
+    .email('Invalid email')
+    .min(1, 'Email is required')
+    .regex(/@/, 'Email address must contain @'),
+  password:z
+  .string()
+  .min(8, 'Password must be at least 8 characters')
+  .max(128, 'Password must be at most 128 characters')
+  .regex(/[A-Z]/, 'Password must contain at least one uppercase letter')
+  .regex(/[a-z]/, 'Password must contain at least one lowercase letter')
+  .regex(/[^A-Za-z0-9]/, 'Password must contain at least one special character')
+});
+
 
   const handleRegister = async () => {
     setLoading(true);
@@ -40,17 +52,21 @@ const SignUpPage = () => {
 
   const handleError = (error: unknown) => {
     if (error instanceof BadRequestError) {
-      setError(error.details ? Object.values(error.details.errors).flat().join(' ') : error.message);
+      setError(
+        error.details
+          ? Object.values(error.details.errors).flat().join(' ')
+          : error.message
+      );
     } else {
       console.error('Unexpected error during registration:', error);
-      setError("An unexpected error occurred. Please try again later.");
+      setError('An unexpected error occurred. Please try again later.');
     }
   };
 
   const handleSuccess = () => {
     router.push('/signin');
-    toast("Account registered", {
-      description: "You can now login with the registered account",
+    toast('Account registered', {
+      description: 'You can now login with the registered account',
       duration: 3000,
       position: 'top-center',
     });
@@ -60,17 +76,12 @@ const SignUpPage = () => {
     event.preventDefault();
     handleRegister();
   };
-  
+
   return (
     <div className="h-full justify-center flex flex-col items-center">
       <div className="flex flex-col gap-4 bg-white p-5 rounded-lg shadow-md w-4/12">
         <h1 className={styles.title}>User Sign Up</h1>
-        {error &&
-          <AlertDestructive 
-            title="Error" 
-            description={error}
-          />
-        }
+        {error && <AlertDestructive title="Error" description={error} />}
         <form onSubmit={handleSubmit}>
           <div className={styles.input_group}>
             <label className={styles.label}>Email:</label>
