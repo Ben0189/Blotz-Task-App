283fcf14099bbd66a6827591b01733d4f11a63bf
Zhichen Ren <zhichenr@student.unimelb.edu.au>
2024-12-09 21:35:16 +1100

    delete AddTask, TaskTable, and taskList that are definded but never used.

--- blotztask-ui/src/app/task-list/page.tsx
+++ blotztask-ui/src/app/task-list/page.tsx
@@ -4,27 +4,11 @@ import { useState } from 'react';
 import { useEffect } from 'react';
 import { Button } from '@/components/ui/button';
 import Link from 'next/link';
-import { AddTask } from './components/add-button';
-import { TaskTable } from './components/task-table';
 import { TaskItemDTO } from '@/model/task-Item-dto';
 import { fetchAllTaskItems } from '@/services/taskService';
 import { Trash } from 'lucide-react';
 
 export default function Page() {
-  const [taskList, setTaskList] = useState<TaskItemDTO[]>([]);
-
-  const loadTasks = async () => {
-    const data = await fetchAllTaskItems();
-    setTaskList(data);
-  };
-
-  /**
-   * Fetch the tasks once and set the hook on the first rendering
-   */
-  useEffect(() => {
-    loadTasks();
-  }, []); // Runs on the first render using [] parameter and rerun when state changes, e.g add task
-
   return (
     <div className="flex  mt-10 mr-10 w-full justify-between">
       <div>
