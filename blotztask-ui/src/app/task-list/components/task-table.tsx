375f9fed112d2dca1e41e8d912154d598d1e6f94
Zhichen Ren <zhichenr@student.unimelb.edu.au>
2024-12-15 23:46:17 +1100

    finish the task that add the task list using card component

--- blotztask-ui/src/app/task-list/components/task-table.tsx
+++ blotztask-ui/src/app/task-list/components/task-table.tsx
@@ -1,46 +1,55 @@
 import {
-  Table,
-  TableBody,
-  TableCell,
-  TableHead,
-  TableHeader,
-  TableRow,
-} from '@/components/ui/table';
+  Card,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from '@/components/ui/card';
+import { Separator } from '@/components/ui/separator';
+// import { TaskItemDTO } from '@/model/task-Item-dto';
 
-import { TaskItemDTO } from '@/model/task-Item-dto';
+// interface TaskTableProps {
+//   tasks: TaskItemDTO[]; // tasks prop is an array of TaskItemDTO objects
+// }
 
-interface TaskTableProps {
-  tasks: TaskItemDTO[]; // tasks prop is an array of TaskItemDTO objects
-}
-
-export function TaskTable({ tasks }: TaskTableProps) {
+export function TaskTable({ tasks }) {
+  console.log('tasks');
+  console.log(tasks);
   return (
-    <Table>
-      <TableHeader>
-        <TableRow>
-          <TableHead className="w-[100px]">Task-id</TableHead>
-          <TableHead>Title</TableHead>
-          <TableHead className="text-right">Isdone</TableHead>
-        </TableRow>
-      </TableHeader>
-      <TableBody>
-        {tasks.map((task) => (
-          <TableRow key={task.id}>
-            <TableCell className="font-medium">{task.id}</TableCell>
-            <TableCell>{task.title}</TableCell>
-            <TableCell className="text-right">
-              {' '}
-              {task.isDone ? 'Yes' : 'No'}
-            </TableCell>
-          </TableRow>
-        ))}
-      </TableBody>
-      {/* <TableFooter>
-          <TableRow>
-            <TableCell colSpan={3}>Total</TableCell>
-            <TableCell className="text-right">$2,500.00</TableCell>
-          </TableRow>
-        </TableFooter> */}
-    </Table>
+    <div className="flex flex-col h-fit mt-10">
+      {tasks.map((task) => (
+        <div key={task.id} className="flex mt-5">
+          <Card
+            className="w-1/3 mr-3 flex justify-center items-center"
+            style={{ backgroundColor: task.label.color }}
+          >
+            <CardHeader>
+              <CardTitle className="text-center bg-task.label.color">
+                {task.label?.name || 'No Label'}
+              </CardTitle>
+            </CardHeader>
+          </Card>
+          <Card
+            className="w-full"
+            style={{ backgroundColor: task.label.color }}
+          >
+            <CardHeader
+              className="ml-5 "
+              style={{
+                backgroundImage:
+                  'linear-gradient(to top, transparent 16.66%, gray 16.66%, gray 83.33%, transparent 83.33%)',
+                backgroundRepeat: 'no-repeat',
+                backgroundSize: '4px 100%',
+                backgroundPosition: '0 0',
+              }}
+            >
+              <CardTitle>{task.title}</CardTitle>
+              <CardDescription>
+                {task?.description || 'NO Description'}
+              </CardDescription>
+            </CardHeader>
+          </Card>
+        </div>
+      ))}
+    </div>
   );
 }
--- blotztask-ui/src/app/task-list/page.tsx
+++ blotztask-ui/src/app/task-list/page.tsx
@@ -7,14 +7,13 @@ import Link from 'next/link';
 import { TaskItemDTO } from '@/model/task-Item-dto';
 import { fetchAllTaskItems } from '@/services/taskService';
 import { Trash } from 'lucide-react';
-
+import { TaskTable } from './components/task-table';
 export default function Page() {
   const [taskList, setTaskList] = useState<TaskItemDTO[]>([]);
 
   const loadTasks = async () => {
     const data = await fetchAllTaskItems();
     setTaskList(data);
-    console.log(taskList);
   };
 
   /**
@@ -23,26 +22,33 @@ export default function Page() {
   useEffect(() => {
     loadTasks();
   }, []); // Runs on the first render using [] parameter and rerun when state changes, e.g add task
+  console.log('taskList');
+  console.log(taskList);
 
   return (
-    <div className="flex  mt-10 mr-10 w-full justify-between">
+    <div className="flex flex-col">
       <div>
-        <p className=" text-5xl font-bold">All Task</p>
-      </div>
-      <div className="flex items-center gap-3">
-        <Link href="/add-task">
-          <Button className="bg-all-task-add-button-bg text-all-task-add-button-text border-2 border-all-task-add-button-border">
-            <span className="">+ </span>Add Task
-          </Button>
-        </Link>
+        <div className="flex  mt-10 mr-10 w-full justify-between">
+          <div>
+            <p className=" text-5xl font-bold">All Task</p>
+          </div>
+          <div className="flex items-center gap-3">
+            <Link href="/add-task">
+              <Button className="bg-all-task-add-button-bg text-all-task-add-button-text border-2 border-all-task-add-button-border">
+                <span className="">+ </span>Add Task
+              </Button>
+            </Link>
 
-        <Button
-          variant="outline"
-          className="bg-all-task-delete-button-bg text-white border-2"
-        >
-          <Trash />
-        </Button>
+            <Button
+              variant="outline"
+              className="bg-all-task-delete-button-bg text-white border-2"
+            >
+              <Trash />
+            </Button>
+          </div>
+        </div>
       </div>
+      <TaskTable tasks={taskList} />
     </div>
   );
 }
