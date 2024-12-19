d3c8db3922a57947eb4d0cfb5b78f1001f616522
Zhichen Ren <zhichenr@student.unimelb.edu.au>
2024-12-19 23:32:34 +1100

    fix layout that after adding three button

--- blotztask-ui/src/app/task-list/page.tsx
+++ blotztask-ui/src/app/task-list/page.tsx
@@ -9,13 +9,11 @@ import { fetchAllTaskItems } from '@/services/taskService';
 import { Trash } from 'lucide-react';
 import { DeleteDialog } from './components/delete-confirmation-dialog';
 
-
-
 import { TaskCard } from './components/task-card';
 
 export default function Page() {
-    const [taskList, setTaskList] = useState<TaskItemDTO[]>([]);
-    const [isDialogOpen, setDialogOpen] = useState(false);
+  const [taskList, setTaskList] = useState<TaskItemDTO[]>([]);
+  const [isDialogOpen, setDialogOpen] = useState(false);
 
   const loadTasks = async () => {
     const data = await fetchAllTaskItems();
@@ -29,43 +27,46 @@ export default function Page() {
     loadTasks();
   }, []); // Runs on the first render using [] parameter and rerun when state changes, e.g add task
 
-
-    return (
-        <div className="flex flex-col items-end mt-10 mr-10">
-
-          <div className="flex  mt-10 mr-10 w-full justify-between">
-            <div>
-              <p className=" text-5xl font-bold">All Task</p>
-            </div>
-            <div className="flex items-center gap-3">
-              <Link href="/add-task">
-                <Button className="bg-all-task-add-button-bg text-all-task-add-button-text border-2 border-all-task-add-button-border">
-                  <span className="">+ </span>Add Task
-                </Button>
-              </Link>
-            </div>
-          </div>
-          
-          <div className="mt-10">
-            <Button asChild className="ml-auto">
-              <Link href="/">Return Home Page</Link>
-            </Button>
-          </div>
-          
-          <Button
-            variant="outline"
-            className="bg-all-task-delete-button-bg text-white border-2"
-            onClick={() => setDialogOpen(true)}
-          >
-            <Trash />
-          </Button>
-
+  return (
+    <div>
+      <div className="flex flex-col items-end mt-10 mr-10">
+        <div className="flex  mt-10 mr-10 w-full justify-between">
           <div>
-            <Button onClick={() => setDialogOpen(true)}>Delete Task</Button>
-            <DeleteDialog isDialogOpen={isDialogOpen} setDialogOpen={setDialogOpen} />
+            <p className=" text-5xl font-bold">All Task</p>
           </div>
-
-          <TaskCard tasks={taskList} />
+          <div className="flex items-center gap-3">
+            <Link href="/add-task">
+              <Button className="bg-all-task-add-button-bg text-all-task-add-button-text border-2 border-all-task-add-button-border">
+                <span className="">+ </span>Add Task
+              </Button>
+            </Link>
+          </div>
         </div>
-      );
+
+        <div className="mt-10">
+          <Button asChild className="ml-auto">
+            <Link href="/">Return Home Page</Link>
+          </Button>
+        </div>
+
+        <Button
+          variant="outline"
+          className="bg-all-task-delete-button-bg text-white border-2"
+          onClick={() => setDialogOpen(true)}
+        >
+          <Trash />
+        </Button>
+
+        <div>
+          <Button onClick={() => setDialogOpen(true)}>Delete Task</Button>
+          <DeleteDialog
+            isDialogOpen={isDialogOpen}
+            setDialogOpen={setDialogOpen}
+          />
+        </div>
+      </div>
+
+      <TaskCard tasks={taskList} />
+    </div>
+  );
 }
