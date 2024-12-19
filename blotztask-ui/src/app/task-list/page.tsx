bf6f4456d6d3851cfdcaa7914c0557618c764291
benjaminneoh <benjaminneoh6343@gmail.com>
2024-12-19 18:07:33 +0800

    implement state changes when different delete button click

--- blotztask-ui/src/app/task-list/page.tsx
+++ blotztask-ui/src/app/task-list/page.tsx
@@ -9,21 +9,10 @@ import Link from 'next/link';
 import { Trash } from 'lucide-react';
 import { DeleteDialog } from './components/delete-confirmation-dialog';
 
-
-
 export default function Page() {
   // const [taskList, setTaskList] = useState<TaskItemDTO[]>([]);
-  // const [isDialogOpen, setDialogOpen] = useState(false);
-  const [isDeleteVisible, setDeleteVisible] = useState(false); // State to toggle visibility
-
-  const loadTasks = async () => {
-    // const data = await fetchAllTaskItems();
-    // setTaskList(data);
-  };
-
-  const toggleDeleteButton = () => {
-    setDeleteVisible((prev) => !prev); // Toggle the visibility of delete button
-  };
+  const [isDialogOpen, setDialogOpen] = useState(false);
+  const [isDeleteTriggered, setIsDeleteTriggered] = useState(false);
 
   /**
    * Fetch the tasks once and set the hook on the first rendering
@@ -32,47 +21,55 @@ export default function Page() {
     loadTasks();
   }, []); // Runs on the first render using [] parameter and rerun when state changes, e.g add task
 
-    return (
-        <div className="flex flex-col items-end mt-5">
+  const loadTasks = async () => {
+    // const data = await fetchAllTaskItems();
+    // setTaskList(data);
+  };
 
-          <div className="flex w-full justify-between">
-            <p className=" text-5xl font-bold">All Task</p>
-            <div className="flex items-center gap-3">
-              <Link href="/add-task">
-                <Button className="bg-all-task-add-button-bg text-all-task-add-button-text border-2 border-all-task-add-button-border">
-                  <span className="">+ </span>Add Task
-                </Button>
-              </Link>
+  const toggleDeleteTrigger = () => {
+    setIsDeleteTriggered((prev) => !prev); 
+  };
 
-              {/* <Button
+  const handleDialogClose = () => {
+    setDialogOpen(false);
+    setIsDeleteTriggered(false);
+  };
+
+  return (
+      <div className="flex flex-col items-end mt-5">
+
+        <div className="flex w-full justify-between">
+          <p className=" text-5xl font-bold">All Task</p>
+          <div className="flex items-center gap-3">
+            <Link href="/add-task">
+              <Button className="bg-all-task-add-button-bg text-all-task-add-button-text border-2 border-all-task-add-button-border">
+                <span className="">+ </span>Add Task
+              </Button>
+            </Link>
+
+            {!isDeleteTriggered ? (
+              // Trash Icon Button (Visible when delete button is not visible)
+              <Button
                 variant="outline"
+                className="bg-all-task-delete-button-bg text-white border-2"
+                onClick={toggleDeleteTrigger}
+              >
+                <Trash />
+              </Button>
+            ) : (
+              // "Delete Task" Button (Visible when trash icon is clicked)
+              <Button
                 className="bg-all-task-delete-button-bg text-white border-2"
                 onClick={() => setDialogOpen(true)}
               >
-                <Trash />
-              </Button> */}
-
-              {!isDeleteVisible ? (
-                // Trash Icon Button (Visible when delete button is not visible)
-                <Button
-                  variant="outline"
-                  className="bg-all-task-delete-button-bg text-white border-2"
-                  onClick={toggleDeleteButton}
-                >
-                  <Trash />
-                </Button>
-              ) : (
-                // "Delete Task" Button (Visible when trash icon is clicked)
-                <Button
-                  className="bg-all-task-delete-button-bg text-white border-2"
-                  onClick={toggleDeleteButton}
-                >
-                  Delete Task
-                </Button>
-              )}
-            </div>
+                Delete Task
+              </Button>
+            )}
           </div>
-      
         </div>
-      );
+
+        <DeleteDialog isDialogOpen={isDialogOpen} setDialogOpen={setDialogOpen} onClose={handleDialogClose}/>
+    
+      </div>
+    );
 }
